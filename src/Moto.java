c6aa9d064763f13d51d68b88ef3b0469af8735f8
Alvaro Dorado <alvaro.dorado@babelgroup.com>
2023-02-23 09:44:29 +0100

    Moto.java creado y terminado

--- src/Moto.java
+++ src/Moto.java
@@ -11,16 +11,26 @@ public class Moto extends Vehiculo implements IVehiculo{
     }
     @Override
     public void fabricarVehiculo(Vehiculo v) {
+        v.setCombustible("Combustible");
+        v.setColor("Negro");
+        v.setDimensiones("2x1.7");
+        v.setMatricula(matriculaVehiculo(v));
+        v.setMotor(true);
+        v.setRuedas(2);
+        v.setPrecio(75000);
+        v.setTipoCambio("Marchas");
+        v.setTipoDireccion("Normal");
 
     }
 
     @Override
     public void montajeVehiculo(Vehiculo v) {
-
+        System.out.println("Moto montada y creada!");
     }
 
     @Override
     public String matriculaVehiculo(Vehiculo v) {
-        return null;
+        String matricula = generarMatricula.generarMatricula();
+        return matricula;
     }
 }
